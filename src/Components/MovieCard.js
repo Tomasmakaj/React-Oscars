21edf15d47400340ac1a4cc0068b425d9f5dd76d
Tomas Makaj <Tmakajcoding@gmail.com>
2022-08-23 13:37:03 -0400

    bootstrap

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,30 +3,39 @@ import React, {useState} from 'react'
 function MovieCard({movie}) {
   const {id, image, nominations, title, description} = movie;
   const [isFront, setIsFront] = useState(true);
-
+  const [isLiked, setIsLiked] = useState(false);
+  const [isWatched, setIsWatched] = useState(false)
   function handleClick() {
     setIsFront((isFront) => !isFront)
-  }
-
+  };
+  function handleLikeClick() {
+    setIsLiked((isLiked) => !isLiked)
+  };
+  function handleWatchedClick() {
+    setIsWatched((isWatched) => !isWatched)
+  };
   return (
     <div id={id} style={{border: "solid 1px black"}}>
       <div className="content">
         <h3 className="header">{title}</h3>
       </div>
       <div onClick={handleClick} className="image">
-        {isFront ? 
+        {isFront ?
           <img src={image} alt={title} height='350px'/> :
           <div>{description}</div>
         }
       </div>
       <div className="extra content">
         <span>
-          <button>♡</button>
-          <button>Add to Watch List</button>
+          <button onClick={handleLikeClick}>
+            {isLiked ? "❤️" : "♡"}
+          </button>
+          <button onClick={handleWatchedClick}>
+            {isWatched ? "In Watch List" : "Add to Watch List"}
+          </button>
         </span>
       </div>
     </div>
   )
 };
-
 export default MovieCard;
--- src/Components/MoviePage.js
+++ src/Components/MoviePage.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import MovieCard from "./MovieCard"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function MoviePage({ movies }) {
-  const renderMovies = movies.map(movie => <MovieCard
+  const renderMovies = movies.map(movie => 
+  <Col sm={2}md={4}>
+  <MovieCard
     key={movie.id}
     movie={movie}
-    />)
+    />
+    </Col>
+  )
   
   return (
-    <div>{renderMovies}</div>
+    <div>
+      <Container>
+        <Row>
+          {renderMovies}
+        </Row>
+      </Container>
+    </div>
   )
 }
 

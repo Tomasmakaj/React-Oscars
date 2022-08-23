bb7b311f0e23d7b86b2cda71afc81935afaf8d41
Tomas Makaj <Tmakajcoding@gmail.com>
2022-08-23 15:18:34 -0400

    NavBar

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,19 +1,24 @@
 import React, {useState} from 'react'
 
 function MovieCard({movie}) {
+  
   const {id, image, nominations, title, description} = movie;
   const [isFront, setIsFront] = useState(true);
   const [isLiked, setIsLiked] = useState(false);
   const [isWatched, setIsWatched] = useState(false)
+  
   function handleClick() {
     setIsFront((isFront) => !isFront)
   };
+  
   function handleLikeClick() {
     setIsLiked((isLiked) => !isLiked)
   };
+  
   function handleWatchedClick() {
     setIsWatched((isWatched) => !isWatched)
   };
+  
   return (
     <div id={id} style={{border: "solid 1px black"}}>
       <div className="content">

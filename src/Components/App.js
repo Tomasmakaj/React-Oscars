80640e8808de062bd6a2234f50393518cfb248ac
Tomas Makaj <Tmakajcoding@gmail.com>
2022-08-24 11:26:06 -0400

    UPDATED

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,4 @@
+
 import '../App.css';
 import { useEffect, useState } from 'react';
 import MoviePage from "./MoviePage"
@@ -6,21 +7,13 @@ import Navbar from './Navbar'
 
 function App() {
   const [movies, setMovies] = useState ([])
-
   const [filter, setFilter] = useState("2021")
-
 
   useEffect(() => {
     fetch("http://localhost:3000/movies")
     .then(res => res.json())
     .then(data => setMovies(data))
   }, []);
-
-
-
-
-
-  return (
 
   const filteredMovies = movies.filter(movie => {
     return movie.year === filter
